c744485a828edb552a7f387bf3611ddc8981987b
techghost01 <pamelalin1761@gmail.com>
2023-05-25 20:15:48 +0100

    initial commit

---
+++ react-check-point/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar'
+import Form from './Components/Form'
+
+function App() {
+
+  return (
+    <div className="app">
+      <Navbar />
+      <Form />
+    </div>
+  )
+}
+
+export default App
+
---
+++ react-check-point/src/Components/Form.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">Name</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">Phone</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">Password</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlTextarea1" className="form-label">Message</label>
+          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+    </div>
+  )
+}
+
+export default Form
